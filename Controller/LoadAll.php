b657d43120bb8154b644a11de4a28a3ecf5df6e0
nguyenvh <41070090+ounhom3antt@users.noreply.github.com>
2019-07-05 18:32:57 +0700

    Change to original

--- Controller/LoadAll.php
+++ Controller/LoadAll.php
@@ -1,22 +1,3 @@
 <?php
-    include_once '../DTO/ChucVu.php';
-    include_once '../DAO/ChucVuDAO.php';
-
-    $dao = new ChucVuDAO();
-
-    $listResult = $dao->loadAll();
-
-    //Parse json UTF-8
-    foreach($listResult as $obj) {
-        if($obj->getId() != null || $obj->getId() != ""){
-            $listResultJSON[] = array(
-                'id' => $obj->getId(),
-                'name' => $obj->getName(),
-            );
-        }
-
-    }
-
-    echo json_encode($listResultJSON, JSON_UNESCAPED_UNICODE);
-
+    
 ?>
--- config/Connection.php
+++ config/Connection.php
@@ -23,24 +23,5 @@ class Connection
             mysqli_close(Connect());
     }
 
-    function execQuery($query){
-        $cn = $this->Connect() or die ('Không thể kết nối tới DataProviderMain');
-        mysqli_set_charset($cn, 'UTF8');
-        $result = $cn->query($query);
-        if (!$result) {
-            die ('Câu truy vấn bị sai');
-        }
-        return $result;
-    }
-
-    function execNonQuery($query){
-        $cn = $this->Connect() or die ('Không thể kết nối tới DataProviderMain');
-        mysqli_set_charset($cn, 'UTF8');
-        if (!mysqli_query($cn, $query))
-            die("Lỗi truy vấn: " . mysqli_error());
-        $affected_rows = mysqli_affected_rows($cn);
-        mysqli_close($cn);
-        return $affected_rows;
-    }
-
+    
 }
